bbb6e697ffd3b31a85a51a078cdac171d76e5acd
Prildy <prildy@gmail.com>
2014-10-15 16:55:24 +0100

    fix connection, adding dummy recharging agent

--- src/asl/agent2.asl
+++ src/asl/agent2.asl
@@ -1,11 +1,27 @@
 // Agent agent2 in project test1
 
 /* Initial beliefs and rules */
+/*visibleEdge_(V1, V2) :- visibleEdge(V1, V2).
+visibleEdge_(V1, V2) :- visibleEdge(V2, V1).
 
+can_walk:- energy(E) & E>1.*/
+
+///////////
+//beliefs  addiction
+///////////
+//+energy(0):
+//	true <-
+//		recharge;
+///		.print("Recharging").
+
+//+step(_) :
+///	can_walk <- visibleEdge_(V1,V2); goto(V1);
+//	.print("I'm going to explore the world!!!"). 
 /* Initial goals */
 
 !start.
 
 /* Plans */
 
-+!start : true <- .print("hello world.").
++!start : true <- recharge; energy(E);
+		.print("recharging, now I have ", E , " energy").
--- src/java/env/Connection.java
+++ src/java/env/Connection.java
@@ -1,50 +1,78 @@
 package env;
 
+import jason.asSyntax.Structure;
+import jason.asSyntax.Term;
 import jason.environment.Environment;
 
 import java.io.IOException;
+import java.util.LinkedList;
+
 import eis.EILoader;
 import eis.EnvironmentInterfaceStandard;
+import eis.exceptions.ActException;
 import eis.exceptions.AgentException;
 import eis.exceptions.ManagementException;
 import eis.exceptions.RelationException;
+import eis.iilang.Action;
+import eis.iilang.Identifier;
+import eis.iilang.Parameter;
 
 public class Connection extends Environment {
 	
 
 	private static final String CN = "massim.eismassim.EnvironmentInterface";
 	private static final String EISMASSIMCONFIG = "eismassimconfig.xml";
-	
+	private EnvironmentInterfaceStandard ei;
 	public void init(String args[]) {
 	
-	EnvironmentInterfaceStandard ei = null;
-	
+		 ei = null;
 		
-	try {
-		ei = EILoader.fromClassName(CN);
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	}
-	for(String e: ei.getEntities()) {
-        try {
-			ei.registerAgent(e);
-            try {
-				ei.associateEntity(e, e);
-			} catch (RelationException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+			
+		try {
+			ei = EILoader.fromClassName(CN);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ei.registerAgent("agent2");
+            
 		} catch (AgentException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+
+		try {
+			ei.associateEntity("agent2", "connectionA1");
+		} catch (RelationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ei.start();
+		} catch (ManagementException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
-	try {
-		ei.start();
-	} catch (ManagementException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
+	
+	@Override
+	public boolean executeAction(String agName, Structure action) {
+		
+		try {
+			System.out.println(agName + " " + action.getFunctor());
+			ei.performAction(agName, actionOf(action));
+		} catch (ActException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
+	
+	private Action actionOf(Structure sAction) {
+		LinkedList<Parameter> parameters = new LinkedList<Parameter>();
+		for (Term term : sAction.getTerms())
+			parameters.add(new Identifier(term.toString()));
+		return new Action(sAction.getFunctor(), parameters);
 	}
 }
